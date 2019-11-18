38539006b6cfd956bd8356b0a0102016542dd750
William Compton <will12@ksu.edu>
2019-11-18 16:25:07 -0600

    Search with filters

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,52 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> Search(string searchString)
+        {
+         
+            List<Movie> result = new List<Movie>();
+
+            foreach(Movie movie in All)
+            {
+               if( movie.Title.Contains(searchString, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    result.Add(movie);
+                }
+            }
+
+            return result;
+        }
+
+        public List<Movie> Filter(List<string> ratings)
+        {
+            List<Movie> result = new List<Movie>();
+
+            foreach(Movie movie in movies)
+            {
+                if (ratings.Contains(movie.MPAA_Rating))
+                {
+                    result.Add(movie);
+                }
+            }
+
+            return result;
+        }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> ratings)
+        {
+
+            List<Movie> result = new List<Movie>();
+
+            foreach(Movie movie in movies)
+            {
+                if (ratings.Contains(movie.MPAA_Rating) && movie.Title.Contains(searchString, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    result.Add(movie);
+                }
+            }
+
+            return result;
+        }
     }
 }
